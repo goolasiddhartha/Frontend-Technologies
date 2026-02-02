e02fba5675717df8dd9ac8aeb048dc4c3ed0b9c8
Siddharthagoola <goolasiddhartha@gmail.com>
2026-02-02 22:38:21 +0530

    props and state in reactjs

--- ReactJS/application_1/src/App.js
+++ ReactJS/application_1/src/App.js
@@ -4,7 +4,10 @@ import Customer from "./Customer.js";
 function App() {
   return (
     <div>
-      <Customer />
+      <br></br>
+      <Customer Cid={"1001"} Cname="Jessica_1" Email="Jessica_1@gmail.com" />
+      <br></br>
+      <Customer Cid={"1002"} Cname="Jessica_2" Email="Jessica_2@gmail.com" />
     </div>
   );
 }
--- ReactJS/application_1/src/Customer.js
+++ ReactJS/application_1/src/Customer.js
@@ -1,7 +1,19 @@
-function Customer() {
+function Customer(props) {
   return (
-    <div>
-      <h1>Welcome to External Css</h1>
+    <div
+      style={{
+        backgroundColor: "black",
+        color: "white",
+        textAlign: "center",
+        borderRadius: "25px",
+        border: "3px solid red",
+      }}
+    >
+      <h1>Customer_Id is :{props.Cid}</h1>
+      <hr></hr>
+      <h2>Customer_Name is:{props.Cname}</h2>
+      <hr></hr>
+      <h3>Customer_Email is:{props.Email}</h3>
     </div>
   );
 }
